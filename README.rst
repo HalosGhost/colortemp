4d692cd03fe71defa6baa9b217a58808323f7c08
halosghost <halosghost@archlinux.info>
2015-11-17 16:53:30 -0600

    Initial Commit

---
+++ README.rst
@@ -0,0 +1,6 @@
+sct
+===
+
+This is my personal fork and copy of Tedu's `sct <http://www.tedunangst.com/flak/post/sct-set-color-temperature>`_.
+
+I cleaned up the code (a bit), pared it down to the necessary parts and may, in the future add some extra features (e.g., querying the current color temperature / a very simple daemon à la redshift).
---
+++ src/sct.c
@@ -0,0 +1,38 @@
+#include "sct.h"
+
+void
+sct_set_color_temp (signed temp) {
+
+    Display * dpy = XOpenDisplay(NULL);
+    signed screen = DefaultScreen(dpy);
+    Window root = RootWindow(dpy, screen);
+    XRRScreenResources * res = XRRGetScreenResourcesCurrent(dpy, root);
+    double ratio = temp % 500 / 500.0;
+
+    #define AVG(c) whitepoints[temp / 500].c * (1 - ratio) + \
+                   whitepoints[temp / 500 + 1].c * ratio
+
+    for ( signed c = 0; c < res->ncrtc; ++ c ) {
+        RRCrtc crtcxid = res->crtcs[c];
+        signed size = XRRGetCrtcGammaSize(dpy, crtcxid);
+        XRRCrtcGamma * crtc_gamma = XRRAllocGamma(size);
+
+        for ( signed i = 0; i < size; ++ i ) {
+            double g = 65535.0 * i / size;
+            crtc_gamma->red[i]   = (unsigned short )(g * AVG(r));
+            crtc_gamma->green[i] = (unsigned short )(g * AVG(g));
+            crtc_gamma->blue[i]  = (unsigned short )(g * AVG(b));
+        } XRRSetCrtcGamma(dpy, crtcxid, crtc_gamma); XFree(crtc_gamma);
+    }
+}
+
+signed
+main (signed argc, char * argv []) {
+
+    signed temp = 6500;
+    if ( argc > 1 ) { temp = atoi(argv[1]); }
+    temp = (temp < 1000 || temp > 10000 ? 6500 : temp) - 1000;
+    sct_set_color_temp(temp);
+    return EXIT_SUCCESS;
+}
+
