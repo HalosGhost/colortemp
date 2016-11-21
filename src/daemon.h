5273d7600b5acac22453a90195a57810f35e10a2
halosghost <halosghost@archlinux.info>
2016-11-20 21:28:33 -0600

    Enable daemon flag (not fully functional yet)

--- src/daemon.h
+++ src/daemon.h
@@ -10,7 +10,7 @@
 #include <time.h>
 #include "colortemp.h"
 
-static const char vos [] = "hrt:";
+static const char vos [] = "dhrt:";
 
 static struct option os [] = {
     { "daemon",     0, 0, 'd' },
@@ -24,6 +24,7 @@ static const char usage [] =
     "Usage: colortempd [options]\n"
     "colortempd -- a simple daemon for handling screen temperature in X\n\n"
     "Options:\n"
+    "  -d, --daemon          Fork off to keep the screen updated\n"
     "  -h, --help            List this help and exit\n"
     "  -r, --reset-temp      Reset the color temperature to the default\n"
     "  -t, --set-temp=NUM    Set the screen's color temperature to NUM\n";
